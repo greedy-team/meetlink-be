cbd6248e485fe863e27f05b559fd3b1afe59b8cd
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2026-02-03 00:58:47 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/greedy/meetlink/MeetlinkApplication.java
@@ -0,0 +1,13 @@
+package com.greedy.meetlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetlinkApplication.class, args);
+	}
+
+}

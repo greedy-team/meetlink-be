3ec3df245574030ca8b68704cb934ea34b61e5ea
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2026-02-05 00:07:40 +0900

    feat: JPA 기본 환경 설정 (#6)
    * feat: JPA 기본 환경 설정
    
    * fix: 코드 컨벤션 적용

---
+++ src/main/java/com/greedy/meetlink/candidate/TimeCandidateRepository.java
@@ -0,0 +1,5 @@
+package com.greedy.meetlink.candidate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeCandidateRepository extends JpaRepository<TimeCandidate, Long> {}
---
+++ src/main/java/com/greedy/meetlink/result/MeetingResultRepository.java
@@ -0,0 +1,5 @@
+package com.greedy.meetlink.result;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeetingResultRepository extends JpaRepository<MeetingResult, Long> {}

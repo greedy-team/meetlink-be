a226b2ba5bb7689da7330b1cfa8fedad7f0a326c
nonactress <tjgsuwls1007@gmail.com>
2026-02-09 17:38:05 +0900

    rename : 컨벤션 반영

--- src/main/java/com/greedy/meetlink/meeting/dto/request/MeetingCreateRequest.java
+++ src/main/java/com/greedy/meetlink/meeting/dto/request/MeetingCreateRequest.java
@@ -8,7 +8,6 @@ import com.greedy.meetlink.meeting.validation.ValidTimeRecommendation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 

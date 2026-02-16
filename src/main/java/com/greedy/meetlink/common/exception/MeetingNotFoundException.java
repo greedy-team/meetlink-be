97d71680e51d1a8915ce00babb2d90a337096a5e
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2026-02-16 23:50:14 +0900

    refactor: API 응답 포맷 개선 (#11)

--- src/main/java/com/greedy/meetlink/common/exception/MeetingNotFoundException.java
+++ src/main/java/com/greedy/meetlink/common/exception/MeetingNotFoundException.java
@@ -1,8 +1,7 @@
 package com.greedy.meetlink.common.exception;
 
 public class MeetingNotFoundException extends RuntimeException {
-
-    public MeetingNotFoundException(String code) {
-        super("모임을 찾을 수 없습니다: " + code);
+    public MeetingNotFoundException() {
+        super("모임을 찾을 수 없습니다.");
     }
 }

b661dbc7ebb55386cea0519e6274c608145c5fd8
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2026-02-25 01:16:47 +0900

    refactor: 패키지 구조 변경

--- src/main/java/com/greedy/meetlink/common/config/HttpClientConfig.java
+++ src/main/java/com/greedy/meetlink/common/config/HttpClientConfig.java
@@ -1,4 +1,4 @@
-package com.greedy.meetlink.config;
+package com.greedy.meetlink.common.config;
 
 import java.time.Duration;
 import org.springframework.context.annotation.Bean;
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
-/** RestClient 공통 설정 - 커넥션/읽기 타임아웃 5초 */
 @Configuration
 public class HttpClientConfig {
-
     @Bean
     public RestClient.Builder restClientBuilder() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
